99a56bf83af03bb9589d52e3b362cb58235be374
sw <sswsan@gmail.com>
2018-12-10 16:34:20 +0800

    inital commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
+            Bye there !!
+          </p>
+          <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
