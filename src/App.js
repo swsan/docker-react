0f55329a457f1655411ea8de12095a7cc6b7ef23
sw <sswsan@gmail.com>
2018-12-11 14:36:44 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bye there !!
+            Bye there - I was chanegd !!
           </p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
